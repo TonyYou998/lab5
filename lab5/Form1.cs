fa4d0ff415566bc1f3ac03ffa2bff9218cf2be3a
TonyYou998 <19520931@gm.uit.edu.com>
2021-06-21 15:10:26 +0700

    bai1

--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -17,5 +17,22 @@ namespace lab5
             InitializeComponent();
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bai3 b3 = new Bai3();
+            b3.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bai1 b1 = new Bai1();
+            b1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bai2 b2 = new Bai2();
+            b2.Show();
+        }
     }
 }
